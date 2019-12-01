008820fdfae2f090e661c1e1a28dc974478a4780
dilly <dylanbarber1@outlook.com>
2019-12-01 14:07:48 -0500

    Add in sql seed data

---
+++ sql/seedData.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+    products (
+        productImage,
+        productName,
+        productPrice,
+        productDescription
+    )
+VALUES
+(
+        '/img/products/1.jpg',
+        'Computer',
+        500,
+        'This is a computer'
+    );
