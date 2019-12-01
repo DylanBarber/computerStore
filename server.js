efa3b58f8d6ad15927b0e7f2928efae5e4050569
dilly <dylanbarber1@outlook.com>
2019-12-01 13:38:25 -0500

    add in products GET route

--- server.js
+++ server.js
@@ -2,8 +2,26 @@
 const express = require('express');
 const app = express(); 
 const path = require('path');
+const mysql = require('mysql');
 
+const sql = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'ecommerce'
+});
 
+sql.connect((err) => {
+    if (err) throw new Error(`Error in SQL connection ${err}`)
+    console.log('SQL connected')
+})
+
+app.get('/api/products', (req, res) => {
+    sql.query('SELECT * FROM products', (err, data) => {
+        if (err) throw new Error(`Error in getting products from DB - ${err}`)
+        res.json(data);
+    })
+})
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname + '/client/build/index.html'));
